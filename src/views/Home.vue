f6ccbe2351ad62905315b1243516b8172877bbb8
Michal Bednarczyk <bednarczyk,business@gmail.com>
2020-03-14 16:54:40 +0100

    added randomizer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,58 @@
 <template>
   <div class="home">
-    <button>Randomize</button>
+    <button v-on:click="randomizeDataset">Randomize</button>
     <canvas width="500" height="500" ref="canvas"></canvas>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 
 type renderDatasetArgs = {
   ctx: CanvasRenderingContext2D;
   dataset: Array<number>;
 };
 
-@Component({
-  components: {}
-})
+@Component
 export default class Home extends Vue {
   private ctx: CanvasRenderingContext2D | null = null;
-  @Prop({ default: () => [1, 5, 9, 14, 22, 3, 2, 1, 1, 6] })
-  dataset!: Array<number>;
+  private canvas: HTMLCanvasElement | null = null;
+  private dataset!: Array<number>;
+
   mounted() {
     const canvas = this.$refs.canvas as HTMLCanvasElement;
+
+    this.dataset = [1, 5, 9, 14, 22, 3, 2, 1, 1, 6];
+    this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
-    if (this.ctx !== null) {
-      this.renderDataset({ ctx: this.ctx, dataset: this.dataset });
-    }
+
+    this.renderDataset();
   }
 
   updated() {
-    if (this.ctx !== null) {
-      this.renderDataset({ ctx: this.ctx, dataset: this.dataset });
-    }
+    this.renderDataset();
   }
 
-  public renderDataset(args: renderDatasetArgs) {
-    const { ctx, dataset } = args;
-    dataset.forEach((val, index) => {
-      const width = 10;
-      ctx.beginPath();
-      ctx.strokeRect(2 * index * width, 0, width, val * 5);
-    });
+  public randomizeDataset(): void {
+    const newDataset = [];
+    for (let index = 0; index < 25; index++) {
+      newDataset.push(Math.random() * 15);
+    }
+    this.dataset = newDataset;
+    this.renderDataset();
+  }
+
+  @Watch("dataset")
+  public renderDataset() {
+    const { ctx, dataset, canvas } = this;
+    if (ctx !== null && canvas !== null) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      dataset.forEach((val, index) => {
+        const width = 10;
+        ctx.beginPath();
+        ctx.strokeRect(2 * index * width, 0, width, val * 5);
+      });
+    }
   }
 }
 </script>
